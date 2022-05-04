ecfa04d1817578d3b0ae13471d4fdab2cde63200
AGHARAMUDHALVI C <agharamudhalvi.c@tigeranalytics.com>
2022-05-04 17:09:00 +0530

    enh-ASSIGNMENT4.1

---
+++ setup.py
@@ -0,0 +1,15 @@
+import setuptools
+
+setuptools.setup(
+
+    name="agharamudhalvi", 
+
+    version="v0.3",
+
+    author="agharamudhalvi",
+
+    packages=setuptools.find_packages(),
+
+    python_requires='>=3.6',
+
+)
